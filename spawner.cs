85ee2727c73e465e2ef5744c48933e881c34a8c5
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2020-12-14 12:59:52 +0530

    Add files via upload

---
+++ spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public float spawnrate = 1f;
+    public GameObject hexagonPrefab;
+    private float nexttimetospawn = 2f;
+    
+    void Update()
+    {
+        if (Time.time >= nexttimetospawn)
+        {
+            Instantiate(hexagonPrefab, Vector3.zero, Quaternion.identity);
+            nexttimetospawn = Time.time + 1f / spawnrate;
+        }
+    }
+}
