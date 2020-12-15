09bdff66fbf442bc7df3c85f7fd3ef38c2359101
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2020-12-15 09:23:20 +0530

    this script makes the enemy follow the protagonist

---
+++ enemyfollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyfollow : MonoBehaviour
+{
+    public float speed;
+    public float minstopdistance=0.5f;
+    public float maxstopdistance=5f;
+    public GameObject player;
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = player.GetComponent<Transform>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) < maxstopdistance && Vector2.Distance(transform.position, target.position) > minstopdistance) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+    }
+}
