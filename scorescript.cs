03ce065ab90bdac1e57236d88b03713fe5e90a2a
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2020-12-26 12:04:37 +0530

    Add files via upload

---
+++ scorescript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class scorescript : MonoBehaviour
+{
+    public static int scorevalue ;
+    public static int hscore;
+    Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = ""+scorevalue/2+"";
+
+
+    }
+}
