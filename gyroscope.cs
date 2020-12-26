8829207c51c670c7fd01de3cb3d180a49af91cc3
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2020-12-26 12:28:48 +0530

    Add files via upload

---
+++ gyroscope.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gyroscope : MonoBehaviour
+{
+	public int level;
+	public int health;
+	public int score;
+	public int hscore;
+	Rigidbody2D rb;
+	float dirX;
+	float moveSpeed = 20f;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		dirX = Input.acceleration.x * moveSpeed;
+		transform.position = new Vector2(Mathf.Clamp(transform.position.x, -7.5f, 7.5f), Mathf.Clamp(transform.position.y, -4.7f, 4.7f));
+	}
+
+	void FixedUpdate()
+	{
+		rb.velocity = new Vector2(dirX, 0f);
+	}
+	public void saveplayer()
+    {
+		savesystem.saveplayer(this);
+    }
+	public void loadplayer()
+    {
+		playerdata data = savesystem.loadplayer();
+		level = data.level;
+		health = data.health;
+		score = data.score;
+		hscore = data.hscore;
+    }
+	
+}
