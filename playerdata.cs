2748c15aaad5b75d05c98b89ecc7b65d7e18ca71
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2020-12-29 20:19:30 +0530

    Add files via upload

---
+++ playerdata.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class playerdata
+{
+    public int level;
+    public int health;
+    public int score;
+    public int hscore;
+    public playerdata(gyroscope player)
+    {
+        level = player.level;
+        health = player.health;
+        score = player.score;
+        hscore = player.hscore;
+        
+    }
+
+}
