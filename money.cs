8b33f1adc928b970c069d9693b43e39bcb30817f
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2020-12-31 14:09:41 +0530

    this is the code for the vallet

---
+++ money.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class money : MonoBehaviour
+{
+    public static int scorevalue;
+    public static int hscore;
+    public static int walet;
+    Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text>();
+       walet = PlayerPrefs.GetInt("walet");
+       walet = scorescript.paisa/2 + walet;
+        //walet = 0;
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = " " + walet;
+        PlayerPrefs.SetInt("walet", walet);
+        scorescript.paisa = 0;
+        //score.text = "" + highscore.hscore + "";
+
+        // score.text = " "+walet;
+        //PlayerPrefs.SetInt("walet", walet);
+    }
+}
