cfc0cbf3a69903672d38793f7f11f4e636a53ac6
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2021-01-05 18:59:41 +0530

    Add files via upload

---
+++ skin2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class skin2 : MonoBehaviour
+{
+    Text amount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        amount = GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("s2") == 1)
+        {if (PlayerPrefs.GetInt("csid") == 2)
+            {
+                amount.text = "selected";
+            }
+        else
+            {
+                amount.text = "select";
+            }
+        }
+        else
+        {
+            amount.text = "600";
+        }
+        
+    }
+}
