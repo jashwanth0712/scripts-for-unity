a087cbd444b9d2a33bb8ea5a305b9e7dde291a63
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2021-04-04 22:39:27 +0530

    changing image in a UI image
    this scripts can change the image in UI image

---
+++ image_changer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class image_changer : MonoBehaviour
+{
+
+    public Sprite ready;
+    public Sprite steady;
+    public Sprite rplus;
+    public Sprite gplus;
+    public Sprite gminus;
+    public Sprite rminus;
+    float t;
+
+    void Start()
+    {
+        
+        Invoke("fready", 0.2f);
+    }
+
+    void fready()
+    {
+        gameObject.GetComponent<Image>().sprite = ready;
+        Invoke("fsteady", 1);
+    }
+
+    void fsteady()
+    {
+        float t = Random.Range(0f, 4f);
+        gameObject.GetComponent<Image>().sprite = steady;
+        Invoke("go", t);
+    }
+    void go()
+    {
+        int rand = Random.Range(0, 4);
+        if (rand == 0)
+        {
+            gameObject.GetComponent<Image>().sprite = rplus;
+            //gameObject.GetComponent<UnityEngine.UI.Image> ().sprite = sprite1;
+        }
+        else if (rand == 1)
+        {
+            gameObject.GetComponent<Image>().sprite = gplus;
+            // gameObject.GetComponent<UnityEngine.UI.Image> ().sprite = sprite2;
+        }
+        else if (rand == 2)
+        {
+            gameObject.GetComponent<Image>().sprite = rminus;
+            //gameObject.GetComponent<UnityEngine.UI.Image> ().sprite = sprite3;
+        }
+        else if (rand == 2)
+        {
+            gameObject.GetComponent<Image>().sprite = gminus;
+            //gameObject.GetComponent<UnityEngine.UI.Image> ().sprite = sprite3;
+        }
+    }
+   
+}
